b00ff0cb7a15ef59ffcdac63cc0b23cac5aa92da
jsheo <tjrwjdgj@gmail.com>
2022-12-27 10:17:38 +0900

    fix: edit .eslintrc.js and remove HelloWorld.vue

--- vue-cli/.eslintrc.js
+++ vue-cli/.eslintrc.js
@@ -1,10 +1,10 @@
 // .eslintrc.js
 module.exports = {
     // 현재 eslintrc 파일을 기준으로 ESLint 규칙을 적용
-    root: true,
+    // root: true,
     // 추가적인 규칙들을 적용
     env: {
-      node: true,
+    //   node: true,
     },
     parserOptions: {
       parser: "babel-eslint",
@@ -19,21 +19,9 @@ module.exports = {
     plugins: ["prettier"],
     // 사용자 편의 규칙 추가
     rules: {
-      "prettier/prettier": [
-        "error",
-        // 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
-        // https://prettier.io/docs/en/options.html
-        {
-          singleQuote: true,
-          semi: true,
-          useTabs: true,
-          tabWidth: 2,
-          trailingComma: "all",
-          printWidth: 80,
-          bracketSpacing: true,
-          arrowParens: "avoid",
-        },
-      ],
       "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+      "vue/multi-word-component-names": ["error", {
+        "ignores": ["a"]
+        }]
     },
   };
--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,33 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- component 명명법 종류
-    <component-temp></component-temp> // kebab-case
-    <ComponentTemp></ComponentTemp> // PascalCase  == <ComponentTemp/>
-     -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/> 
+<!-- template root 는 하나의 엘레멘트만 있어야 함 -->
+  <div>
+    App Start 
+    {{ str }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue' // 컴포넌트 내용을 들고 와서 사용하는 부분
-
 export default {
-  // 인스턴스 옵션 속성 또는 컴포넌트 옵션 속성을 넣어주는 곳
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      str : 'hi'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
